9d4ccd625468d89b35486a2fca2282f2a261858e
Waleeddarwesh <Waleeddarwesh2002@gmail.com>
2024-12-11 04:54:44 +0200

    udate requirements

--- Handcrafts/settings.py
+++ Handcrafts/settings.py
@@ -210,8 +210,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+# You may want to define the location for static files (for production)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
