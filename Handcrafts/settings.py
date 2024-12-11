7e4baac6cb6ec5038a26ff6929363f45f5aea811
Waleeddarwesh <Waleeddarwesh2002@gmail.com>
2024-12-11 02:38:36 +0200

    edit database settings

--- Handcrafts/settings.py
+++ Handcrafts/settings.py
@@ -159,7 +159,7 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('postgresql://postgres:NXDGelQcZNBmSVsfEPHpgwAQZMKoLBOe@junction.proxy.rlwy.net:58374/railway'))
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
