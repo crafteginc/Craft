d11d75b166cb7b9024f5bbf2a2ab83a5cacd1c08
hesh12343 <heshamashraf577@gmail.com>
2025-01-01 07:09:03 +0200

    add middleware

--- accounts/utils.py
+++ accounts/utils.py
@@ -2,8 +2,15 @@ import random
 import asyncio
 from django.core.mail import EmailMessage
 from django.conf import settings
+import requests
 from .models import User, OneTimePassword
-
+import requests
+from google.auth.transport import requests
+from google.oauth2 import id_token
+from accounts.models import User
+from django.contrib.auth import authenticate
+from django.conf import settings
+from rest_framework.exceptions import AuthenticationFailed
 # دالة غير متزامنة لإرسال OTP عبر البريد الإلكتروني
 async def send_generated_otp_to_email(email, request):
     subject = "One time Passcode for Email Verification"
@@ -43,49 +50,49 @@ async def send_normal_email(data):
     await asyncio.to_thread(email.send)
 
 
-# class Google():
-#     @staticmethod
-#     def validate(access_token):
-#         try:
-#             id_info=id_token.verify_oauth2_token(access_token, requests.Request())
-#             if 'accounts.google.com' in id_info['iss']:
-#                 return id_info
-#         except:
-#             return "the token is either invalid or has expired"
+class Google():
+    @staticmethod
+    def validate(access_token):
+        try:
+            id_info=id_token.verify_oauth2_token(access_token, requests.Request())
+            if 'accounts.google.com' in id_info['iss']:
+                return id_info
+        except:
+            return "the token is either invalid or has expired"
 
-# def register_social_user(provider, email, first_name, last_name):
-#     old_user=User.objects.filter(email=email)
-#     if old_user.exists():
-#         if provider == old_user[0].auth_provider:
-#             register_user=authenticate(email=email, password=settings.SOCIAL_AUTH_PASSWORD)
+def register_social_user(provider, email, first_name, last_name):
+    old_user=User.objects.filter(email=email)
+    if old_user.exists():
+        if provider == old_user[0].auth_provider:
+            register_user=authenticate(email=email, password=settings.SOCIAL_AUTH_PASSWORD)
 
-#             return {
-#                 'full_name':register_user.get_full_name,
-#                 'email':register_user.email,
-#                 'tokens':register_user.tokens()
-#             }
-#         else:
-#             raise AuthenticationFailed(
-#                 detail=f"please continue your login with {old_user[0].auth_provider}"
-#             )
-#     else:
-#         new_user={
-#             'email':email,
-#             'first_name':first_name,
-#             'last_name':last_name,
-#             'password':settings.SOCIAL_AUTH_PASSWORD
-#         }
-#         user=User.objects.create_user(**new_user)
-#         user.auth_provider=provider
-#         user.is_verified=True
-#         user.save()
-#         login_user=authenticate(email=email, password=settings.SOCIAL_AUTH_PASSWORD)
+            return {
+                'full_name':register_user.get_full_name,
+                'email':register_user.email,
+                'tokens':register_user.tokens()
+            }
+        else:
+            raise AuthenticationFailed(
+                detail=f"please continue your login with {old_user[0].auth_provider}"
+            )
+    else:
+        new_user={
+            'email':email,
+            'first_name':first_name,
+            'last_name':last_name,
+            'password':settings.SOCIAL_AUTH_PASSWORD
+        }
+        user=User.objects.create_user(**new_user)
+        user.auth_provider=provider
+        user.is_verified=True
+        user.save()
+        login_user=authenticate(email=email, password=settings.SOCIAL_AUTH_PASSWORD)
        
-#         tokens=login_user.tokens()
-#         return {
-#             'email':login_user.email,
-#             'full_name':login_user.get_full_name,
-#             "access_token":str(tokens.get('access')),
-#             "refresh_token":str(tokens.get('refresh'))
-#         }
+        tokens=login_user.tokens()
+        return {
+            'email':login_user.email,
+            'full_name':login_user.get_full_name,
+            "access_token":str(tokens.get('access')),
+            "refresh_token":str(tokens.get('refresh'))
+        }
 
