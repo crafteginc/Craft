89ed21ba901cd0afbc1af4116fd1877c9c43e25b
Waleeddarwesh <Waleeddarwesh2002@gmail.com>
2025-01-01 23:04:23 +0200

    Revert "edit sent email"
    This reverts commit 5345e53518caef2beb07b2d97b458691b24ec53c.

--- accounts/utils.py
+++ accounts/utils.py
@@ -21,9 +21,8 @@ def send_generated_otp_to_email(email, request):
 
     Thanks for signing up on CraftEG! 
 
-    Please verify your email using the    
-    following One-Time Passcode 
-    (OTP): {otp}
+    Please verify your email using    the following One-Time Passcode (OTP): 
+    {otp}
 
     Best regards,  
     The CraftEG Team
