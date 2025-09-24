764ef4aa345669b824a49ed170e60349b13e3fb8
Waleed Darwesh <Waleeddarwesh2002@gmail.com>
2025-09-24 19:19:43 +0300

    edit asgi

--- Handcrafts/asgi.py
+++ Handcrafts/asgi.py
@@ -1,26 +1,19 @@
 import os
 from django.core.asgi import get_asgi_application
-
 from channels.routing import ProtocolTypeRouter, URLRouter
-
-from notifications.routing import websocket_urlpatterns as notifications_ws_urlpatterns
 from chatapp.routing import websocket_urlpatterns as chatapp_ws_urlpatterns
+from notifications.routing import websocket_urlpatterns as notifications_ws_urlpatterns
 from .midleware import TokenAuthMiddleware
 
-# This line correctly sets the settings module for production
-settings_module = 'Handcrafts.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'Handcrafts.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
-# This is the simplified and corrected application setup
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Handcrafts.settings')
+
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         "websocket": TokenAuthMiddleware(
             URLRouter(
-                [
-                    *notifications_ws_urlpatterns,
-                    *chatapp_ws_urlpatterns,
-                ]
+                chatapp_ws_urlpatterns + notifications_ws_urlpatterns
             )
         ),
     }
