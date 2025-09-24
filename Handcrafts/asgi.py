f6f49b05568007dbbe33036302c6ef714a28aee6
Waleed Darwesh <Waleeddarwesh2002@gmail.com>
2025-09-24 20:01:32 +0300

    edit asgi

--- Handcrafts/asgi.py
+++ Handcrafts/asgi.py
@@ -1,12 +1,16 @@
 import os
 from django.core.asgi import get_asgi_application
+
+# Set the settings module for Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Handcrafts.settings')
+
+# Initialize the Django application BEFORE importing routing and middleware.
+application = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chatapp.routing import websocket_urlpatterns as chatapp_ws_urlpatterns
 from notifications.routing import websocket_urlpatterns as notifications_ws_urlpatterns
 from .midleware import TokenAuthMiddleware
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Handcrafts.settings')
 
 application = ProtocolTypeRouter(
     {
