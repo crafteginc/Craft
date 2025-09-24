7cde08aa4dfa533f23e91314ff0820dd0740e164
Waleed Darwesh <Waleeddarwesh2002@gmail.com>
2025-09-24 19:40:05 +0300

    edit wsgi

--- Handcrafts/wsgi.py
+++ Handcrafts/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-settings_module='Handcrafts.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'Handcrafts.settings'
+settings_module='Handcrafts.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module)
 application = get_wsgi_application()
