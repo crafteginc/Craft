e99273950c07f82861ce5a1cc8cc740c7d4fa4fd
Waleed Darwesh <Waleeddarwesh2002@gmail.com>
2025-09-27 18:19:30 +0300

    edit redirect protocol to https

--- Handcrafts/settings.py
+++ Handcrafts/settings.py
@@ -133,6 +133,12 @@ MIDDLEWARE = [
 ]
 ROOT_URLCONF = 'Handcrafts.urls'
 
+if ENVIRONMENT != 'development':
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
